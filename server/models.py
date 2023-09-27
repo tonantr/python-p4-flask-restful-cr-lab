5b410b029eaba1deb854692c42f693f17135db88
tonan <126637328+tonantr@users.noreply.github.com>
2023-09-27 11:11:25 -0400

    completed REST Create and Retrieve Lab

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import DECIMAL
 
 db = SQLAlchemy()
 
@@ -7,3 +8,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(DECIMAL)
+
+    def __repr__(self) -> str:
+        return f"Plant(name='{self.name}', image='{self.image}', price='{self.price}')"
